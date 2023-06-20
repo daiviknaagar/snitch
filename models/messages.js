bb45ae279cd450e27ba4267d2a68e5bf8f82c65f
daiviknaagar <121276301+daiviknaagar@users.noreply.github.com>
2023-06-20 12:28:30 +0530

    Delete models directory

--- models/messages.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const messageSchema = new mongoose.Schema({
-    grp: String,
-    content: [],
-});
-
-module.exports = mongoose.model('Message', messageSchema);
